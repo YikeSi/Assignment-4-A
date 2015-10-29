68f2db28789749a858fb66d295ef8766d2ccc6b9
YikeSi <si.y@husky.neu.edu>
2015-10-29 14:32:15 -0400

    v

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 var margin = {t:50,r:125,b:50,l:125};
-var width = document.getElementById('plot') - margin.r - margin.l,
-    height = document.getElementById('plot') - margin.t - margin.b;
+var width = document.getElementById('plot').clientWidth - margin.r - margin.l,
+    height = document.getElementById('plot').clientHeight - margin.t - margin.b;
 
 var canvas = d3.select('.plot')
     .append('svg')
@@ -11,38 +11,99 @@ var canvas = d3.select('.plot')
     .attr('transform','translate('+margin.l+','+margin.t+')');
 
 //Scale for the size of the circles
-var scaleR = d3.scale.sqrt().domain([5,100]).range([5,120]);
+var scaleR = d3.scale.sqrt().domain([0,100]).range([5,120]);
 
 
 d3.csv('data/olympic_medal_count.csv', parse, dataLoaded);
 
 function dataLoaded(err,rows){
 
-    var year = 1900;
-    rows.sort(function(a,b){
-        //Note: this is called a "comparator" function
-        //which makes sure that the array is sorted from highest to lowest
-        return b[year] - a[year];
-    });
-
-    //Note: this returns "top5" as a subset of the larger array "rows", containing positions 0,1,2,3,4
-    var top5 = rows.slice(0,5);
-
-    //Call the draw function
-    draw(top5, year);
-
     //TODO: fill out this function
-    d3.selectAll('.btn-group .year').on('click',function(){
+    d3.selectAll('.btn-group .year')
+        .on('click', function() {
 
-        var year = d3.select(this).id();
-        console.log("Show top 5 medal count for: " + year);
-    });
+            var year = d3.select(this).attr('id');
+
+            if (year == 'year-1900') {
+
+                var y1900 = 1900;
+                rows.sort(function(a,b){
+                    return b[y1900] - a[y1900];
+                });
+                var top5 = rows.slice(0,5);
+                draw(top5, y1900);
+            }
+            else if (year == 'year-1960') {
+
+                var y1960 =  1960;
+                rows.sort(function(a,b){
+                    return b[y1960] - a [y1960];
+                });
+                var top5 = rows.slice(0,5);
+                draw(top5, y1960);
+            }
+            else {
+
+                var y2012 = 2012;
+                rows.sort(function(a,b){
+                    return b[y2012] - a[y2012];
+                });
+                var top5 = rows.slice(0,5);
+                draw(top5, y2012);
+            }
+            console.log(top5);
+        }
+    )
+
 }
+
+
+
 
 function draw(rows, year){
     //TODO: Complete drawing function, accounting for enter, exit, update
     //Note that this function requires two parameters
     //The second parameter, "year", determines which one of the three years (1900,1960,2012) to draw the medal counts based on
+    var node = canvas.selectAll('.node')
+                    .data(rows, function(d){return d.country});
+    var    nodeenter = node.enter()
+            .append("g")
+            .attr('class','node')
+            nodeenter
+                .append('circle')
+                .attr('r',0)
+                .style('fill','rgb(255, 153, 51)')
+                .style('stroke','rgb(153, 204, 0)')
+                .style('stroke-width',4);
+            nodeenter
+                .append('text')
+                .text(function(d){
+                    return d.country
+                });
+    node.exit()
+        .remove();
+
+    node.transition()
+        .duration(700)
+        .delay(function(d, i) { return i *700; })
+        .select('circle')
+        .attr('transform',function(d,i){return 'translate('+i*220+','+height/6+')';})
+        .attr('cy',60)
+        //.attr('cx',function(d,i){
+        //    return i*220;
+        //})
+        .attr('r',function(d){
+            return scaleR(d[year])/2;
+        });
+    node.transition()
+        .duration(1000).select('text')
+        .delay(function(d, i) { return i *700; })
+        .attr('transform',function(d,i){return 'translate('+i*220+','+height/9+')';})
+        //node.append('text')
+        //    .text(function (d){
+        //    return d.country;
+        //})
+
 }
 
 function parse(row){
